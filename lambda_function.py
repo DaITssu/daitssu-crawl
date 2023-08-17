38771ef60e446b332465fe1c405019b41aae6073
k0000k <locker50@naver.com>
2023-08-17 22:52:19 +0900

    feat: fix lambda_handler using json

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,14 @@
+import json
+
 from smart_campus import smart_campus_crawling
 from departments import departments_crawling
 from fun_system import fus_system_crawling
 from ssu_catch import ssu_catch_crawling
 
 def lambda_handler(event, context):
-    function_name = event['function']
-    value = event['value']
+    body = json.loads(event['body'])
+    function_name = body['function']
+    value = body['value']
     
     if function_name == 'smart_campus':
         result = smart_campus_crawling(value)
