84cf068c039b9852da060283fab56559ee289896
JustMinEEE <destro1040@gmail.com>
2023-09-15 01:04:19 +0900

    [feat] ai융합학부 공지사항 크롤링 코드 추가

--- departments.py
+++ departments.py
@@ -2,6 +2,7 @@ import bs4.element
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
+from datetime import datetime
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, CHAR, ARRAY, DateTime
@@ -9,6 +10,7 @@ import sqlalchemy
 import dev_db
 
 URL = "http://cse.ssu.ac.kr/03_sub/01_sub.htm"
+AI_BASE_URL = "http://aix.ssu.ac.kr/"
 
 Base = declarative_base()
 
@@ -87,6 +89,91 @@ class ComputerNotification(Base):
         ))
 
 
+class AiNotification(Base):
+    __tablename__ = "notice"
+    # schema를 notice로 하면 컴퓨터 학부와 겹쳐서 error가 발생합니다.
+    # ai_notice를 만들어야 할 것 같습니다. 
+    __table_args__ = {"schema": "notice"}
+    id = Column(Integer, primary_key=True)
+    title = Column(CHAR(1024))
+    department_id = Column(Integer)
+    content = Column(CHAR(2048))
+    category = Column(CHAR(32))
+    image_url = Column(ARRAY(CHAR(2048)))
+    file_url = Column(ARRAY(CHAR(2048)))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    def __init__(self, row: bs4.element.Tag):
+        childrens = row.find_all("td")
+
+        if childrens:
+            href = childrens[0].find("a")["href"]
+            self.__link = AI_BASE_URL + href
+        else:
+            return
+
+        req = requests.get(self.__link)
+        soup = BeautifulSoup(req.text, "lxml")
+        contents = soup.find("table", class_="table").find_all("p")
+
+        # 제목
+        self.title = childrens[0].text.strip()
+
+        # 내용
+        self.content = ""
+        for content in contents:
+            self.content += content.text
+
+        # 카테고리
+        self.category = "AI융합학부"
+
+        # 이미지
+        self.image_url = []
+
+        # 파일
+        file_link = []
+
+        contents = soup.find("table", class_="table").find_all("li")
+        for content in contents:
+            link_tag = content.find("a")
+            file_link.append(AI_BASE_URL + link_tag["href"])
+
+        self.file_url = file_link
+
+        # 생성 시각
+        created_date = list(map(int, childrens[2].text.split(".")))
+        self.created_at = date(created_date[0], created_date[1], created_date[2])
+
+        # 업데이트 시각
+        self.updated_at = datetime.now().strftime("%Y-%m-%d")
+
+        with engine.connect() as connect:
+            department_table = Table(
+                "department", metadata_obj, schema="main", autoload_with=engine
+            )
+            # department_table에 ai융합학부가 없는 듯 합니다.
+            query = department_table.select().where(department_table.c.name == "ai융합학부")
+            results = connect.execute(query)
+            for result in results:
+                self.department_id = result.id
+
+    def __str__(self):
+        return (
+            "title: {0}\n"
+            "content: {1}\n"
+            "image_url: {2}\n"
+            "file_url: {3}\n"
+            "department_id: {4}".format(
+                self.title,
+                self.content,
+                self.image_url,
+                self.file_url,
+                self.department_id,
+            )
+        )
+    
+
 def computer_department_crawling(value):
     page = 1  # 1 ~
     base_url = URL + "?page={0}".format(page)
@@ -103,9 +190,30 @@ def computer_department_crawling(value):
             session.add(result)
         session.commit()
 
+def ai_department_crawling(value):
+    page = 1
+    url = AI_BASE_URL + "notice.html?searchKey=ai"
+    # 페이지 옵션: &page=
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    content = soup.find("table", class_="table")
+    rows = content.find_all("tr")
+    results = []
+
+    for row in rows[1:]:
+        results.append(AiNotification(row))
+
+    with session_maker() as session:
+        for result in results:
+            session.add(result)
+            # print(result)  # db 삽입 내용 확인 출력문
+
+        session.commit()
+
 
 def departments_crawling(value):
     computer_department_crawling(value)
+    ai_department_crawling(value)
 
 if __name__ == "__main__":
     departments_crawling(1)
